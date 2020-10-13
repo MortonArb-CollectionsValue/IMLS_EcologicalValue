ce5423e70ab7adeb9def216ab3501d96c18003f6
ShivenS04 <shivenrun@gmail.com>
2020-10-13 16:24:02 -0500

    Species Occurrence Points Progress
    Completed steps 1-3 from Workplan

--- Shiven_Species_Occurrence_Points.R
+++ Shiven_Species_Occurrence_Points.R
@@ -1,22 +1,32 @@
 library(ggplot2)
 library(rgdal); library(sp); library(raster)
+library(maps)
 
-# test.spp <- read.csv("Desktop")
-# summary(test.spp)
-# 
-# # Quick plot of where these things are
-# map.world <- map_data("world")
-# 
-# # Convert test.spp to a spatial poitns data frame
-# spp.sp <- SpatialPointsDataFrame(coords=test.spp[,c("decimalLongitude", "decimalLatitude")], data=test.spp, proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs"))
-# summary(spp.sp)
-# class(spp.sp)
-# 
+#downloaded individual file to computer instead of using whole folder on hard drive
+#didn't work when I tried to download from hard drive
+test.spp <- read.csv("~/Quercus_lyrata.csv")
+summary(test.spp)
+head(test.spp)
+
+# Quick plot of where these things are
+map.world <- map_data("world")
+
+
+# Convert test.spp to a spatial poitns data frame
+spp.sp <- SpatialPointsDataFrame(coords=test.spp[,c("decimalLongitude", "decimalLatitude")], data=test.spp, proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs"))
+summary(spp.sp)
+class(spp.sp)
+
 # # Readign in the ecoregion file
-# ecos <- readOGR("~/Downloads/official/wwf_terr_ecos.shp")
-# summary(ecos)
-# names(ecos)
-# class(ecos)
-# 
+ecos <- readOGR("D:/official/wwf_terr_ecos.shp")
+summary(ecos)
+names(ecos)
+class(ecos)
+
+# ggplot(data=ecos[ecos$BIOME==14,]) +
+#   coord_equal() +
+#   geom_path(data=map.world, aes(x=long, y=lat, group=group)) +
+#   geom_polygon(aes(x=long, y=lat))
+#
 # # This bonks because we're trying to do too much; read into it and ask questions
 # test.extract <- extract(ecos, spp.sp, method="simple")
