1734a784cdbe5935daee4fb34b77d9bd1a30d904
Christy Rollinson <crollinson@mortonarb.org>
2020-10-14 16:29:46 -0500

    SOILS: example of merging data together
    note: I think I probably have the code extraction set up wrong, but it's at least a start that can be used to modify fields as appropriate

--- CodeSnippets.R
+++ CodeSnippets.R
@@ -9,35 +9,10 @@ spp.species <- dir("/Volumes/GoogleDrive/Shared drives/IMLS MFA/occurrence_point
 
 test.spp <- read.csv("/Volumes/GoogleDrive/Shared drives/IMLS MFA/occurrence_points/outputs/spp_raw_points/Quercus_lyrata.csv")
 summary(test.spp)
-
-
-# Reading in the rasterized HWSD downloaded from here: http://www.fao.org/soils-portal/soil-survey/soil-maps-and-databases/harmonized-world-soil-database-v12/en/ 
-# Good news: it reads in; bad news: I have no idea what it means
-# I THINK these are soil codes that crosswalk with values in the database (.mdb) file that can be downloaded in the same place as the raster
-
-hwsd <- raster("~/Downloads/HWSD_RASTER/hwsd.bil")
-projection(hwsd) <- CRS("+proj=longlat +datum=WGS84 +no_defs")
-hwsd
-
-# Trying to read the access database
-# https://stackoverflow.com/questions/37912560/programmatically-read-access-mdb-files-into-r-for-both-windows-and-mac
-# https://stackoverflow.com/questions/23568899/access-data-base-import-to-r-installation-of-mdb-tools-on-mac
-library("Hmisc")
-df <- mdb.get("~/Downloads/HWSD.mdb")
-summary(df$HWSD_SMU)
-
-
-spp.sp$hwsd_code <- extract(hwsd, spp.sp)
+# Convert test.spp to a spatial poitns data frame
+spp.sp <- SpatialPointsDataFrame(coords=test.spp[,c("decimalLongitude", "decimalLatitude")], data=test.spp, proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs"))
 summary(spp.sp)
-
-
-# db <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
-#                         DBQ=~/Downloads/HWSD.mdb")
-# data <- as_tibble(sqlFetch(db, "", rownames=T))
-
-# 
-for200 <- raster("~/Desktop/FOR_2000.asc")
-for200
+class(spp.sp)
 
 # Quick plot of where these things are
 map.world <- map_data("world")
@@ -48,11 +23,53 @@ ggplot(data=test.spp) +
   geom_point(aes(x=decimalLongitude, y=decimalLatitude), color="red")
 
 
-# Convert test.spp to a spatial poitns data frame
-spp.sp <- SpatialPointsDataFrame(coords=test.spp[,c("decimalLongitude", "decimalLatitude")], data=test.spp, proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs"))
-summary(spp.sp)
-class(spp.sp)
+# Reading in the rasterized HWSD downloaded from here: http://www.fao.org/soils-portal/soil-survey/soil-maps-and-databases/harmonized-world-soil-database-v12/en/ 
+# Good news: it reads in; bad news: I have no idea what it means
+# I THINK these are soil codes that crosswalk with values in the database (.mdb) file that can be downloaded in the same place as the raster
+# NOTE that for all fo these things, we'll need to assing a coordinate reference system (CRS; aka projection)
+hwsd <- raster("~/Downloads/HWSD_RASTER/hwsd.bil")
+projection(hwsd) <- CRS("+proj=longlat +datum=WGS84 +no_defs")
+hwsd
 
+# Extract the HWSD soil code from the master raster
+spp.sp$hwsd_code <- extract(hwsd, spp.sp)
+summary(spp.sp)
+
+# Now we'll need to wade through the many-leveled mess that is a database.
+# I *THINK* the hwsd_code I extracted above is the "ID" OR "MU.GLOBAL" value in the HWSD_SMU data table.  These will probably get crosswalked with SU.SYMBOL and SU.CODE to get important data.  We shoudl start by trhing to get AWC (available water capactiy); ROOTS (rooting depth?) and TEXTURE values out
+
+# Trying to read the access database
+# https://stackoverflow.com/questions/37912560/programmatically-read-access-mdb-files-into-r-for-both-windows-and-mac
+# https://stackoverflow.com/questions/23568899/access-data-base-import-to-r-installation-of-mdb-tools-on-mac
+library("Hmisc")
+df <- mdb.get("~/Downloads/HWSD.mdb")
+summary(df)
+summary(df$HWSD_SMU)
+df$D_TEXTURE
+head(df$HWSD_DATA)
+df$D_AWC
+df$D_SYMBOL
+df$D_SYMBOL90
+
+# Now we'll use the code we extracted to crosswalk things
+#NOTE NOTE NOTE: I COULD HAVE THE COLUMN CROSSWALK WRONG
+soil.spp1 <- df$HWSD_DATA[df$HWSD_DATA$ID %in% unique(spp.sp$hwsd_code),]
+soil.spp <- merge(spp.sp[,c("decimalLongitude", "decimalLatitude", "database", "UID", "hwsd_code")], soil.spp1, by.x="hwsd_code", by.y="ID", all.x=T)
+dim(soil.spp)
+summary(soil.spp)
+
+# db <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
+#                         DBQ=~/Downloads/HWSD.mdb")
+# data <- as_tibble(sqlFetch(db, "", rownames=T))
+
+# 
+for200 <- raster("~/Desktop/FOR_2000.asc")
+for200
+
+
+# ----------------------------------
+# OLD STUFF
+# ----------------------------------
 # Readign in the ecoregion file
 ecos <- readOGR("~/Downloads/official/wwf_terr_ecos.shp")
 summary(ecos)
