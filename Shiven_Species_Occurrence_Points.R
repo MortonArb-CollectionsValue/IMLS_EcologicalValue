7bfb6c18455b13c401b1a19726446e269b440743
ShivenS04 <shivenrun@gmail.com>
2020-10-14 14:02:26 -0500

    Pre-Meeting Changes
    made example code for extracting and spTransform

--- Shiven_Species_Occurrence_Points.R
+++ Shiven_Species_Occurrence_Points.R
@@ -22,11 +22,17 @@ ecos <- readOGR("D:/official/wwf_terr_ecos.shp")
 summary(ecos)
 names(ecos)
 class(ecos)
+head(ecos)
 
-# ggplot(data=ecos[ecos$BIOME==14,]) +
-#   coord_equal() +
-#   geom_path(data=map.world, aes(x=long, y=lat, group=group)) +
-#   geom_polygon(aes(x=long, y=lat))
-#
-# # This bonks because we're trying to do too much; read into it and ask questions
-# test.extract <- extract(ecos, spp.sp, method="simple")
+#works up until here
+test.spp.package <- spTransform(ecos, CRS=CRS(spp.sp))
+#This bonks because we're trying to do too much; read into it and ask questions
+test.extract <- extract(ecos, spp.sp, method="simple")
+
+#Change df and FileName to actual things
+write.csv(df, "FileName.csv", row.names = FALSE)
+
+ggplot(data=ecos[ecos$BIOME==14,]) +
+   coord_equal() +
+   geom_path(data=map.world, aes(x=long, y=lat, group=group)) +
+   geom_polygon(aes(x=long, y=lat))
