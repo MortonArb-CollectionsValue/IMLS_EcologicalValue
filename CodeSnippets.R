a4b74ecb7b530e1c4e35ab49496dcf450daf614e
Christy Rollinson <crollinson@mortonarb.org>
2020-11-04 15:27:21 -0600

    Updates on extraction loop
    if/when we do weighted averages, we'll need to rejigger, but this should work as a start

--- CodeSnippets.R
+++ CodeSnippets.R
@@ -27,7 +27,7 @@ class(spp.sp)
 # Good news: it reads in; bad news: I have no idea what it means
 # I THINK these are soil codes that crosswalk with values in the database (.mdb) file that can be downloaded in the same place as the raster
 # NOTE that for all fo these things, we'll need to assing a coordinate reference system (CRS; aka projection)
-hwsd <- raster("~/Downloads/HWSD_RASTER/hwsd.bil")
+hwsd <- raster("~/Desktop/HWSD_RASTER/hwsd.bil")
 projection(hwsd) <- CRS("+proj=longlat +datum=WGS84 +no_defs")
 hwsd
 
@@ -50,11 +50,15 @@ length(unique(spp.sp$hwsd_code))
 unique(spp.sp$hwsd_code)
 
 library("Hmisc")
-df <- mdb.get("~/Downloads/HWSD.mdb")
+df <- mdb.get("~/Desktop/HWSD.mdb")
 summary(df)
 summary(df$D_ADD_PROP)
 summary(df$HWSD_SMU)
 summary(df$HWSD_DATA)
+head(df$HWSD_DATA)
+
+head(df$HWSD_SMU)
+
 df$D_TEXTURE
 df$D_AWC
 df$D_SYMBOL
@@ -62,10 +66,52 @@ df$D_SYMBOL90
 
 # Now we'll use the code we extracted to crosswalk things
 #NOTE NOTE NOTE: I COULD HAVE THE COLUMN CROSSWALK WRONG
-soil.spp1 <- df$HWSD_DATA[df$HWSD_DATA$ID %in% unique(spp.sp$hwsd_code),]
-soil.spp <- merge(spp.sp[,c("decimalLongitude", "decimalLatitude", "database", "UID", "hwsd_code")], soil.spp1, by.x="hwsd_code", by.y="ID", all.x=T)
+soil.spp1 <- df$HWSD_DATA[df$HWSD_DATA$MU.GLOBAL %in% unique(spp.sp$hwsd_code),]
+# soil.spp <- merge(spp.sp[,c("decimalLongitude", "decimalLatitude", "database", "UID", "hwsd_code")], soil.spp1, by.x="hwsd_code", by.y="MU.GLOBAL", all.x=T)
+head(soil.spp1)
 dim(soil.spp)
 summary(soil.spp)
+
+
+summary(spp.sp)
+
+# Vars we want from HWSD_DATA
+vars.hwsd <- c("SHARE", "T.SAND", "T.SILT", "T.CLAY", "SU.SYM74", "AWC.CLASS", "T.PH.H2O", "T.OC", "T.CEC.SOIL", "T.BULK.DENSITY")
+# spp.sp[,vars.hwsd] <- NA
+for(MU in unique(spp.sp$hwsd_code)){
+  # Figuring out which points this soil type applies to
+  rows.mu <- which(spp.sp$hwsd_code==MU)
+  
+  soil.now <- df$HWSD_DATA[df$HWSD_DATA$MU.GLOBAL==MU,]
+  
+  # Find out what is the dominant soil condition
+  soil.most <- which(soil.now$SHARE==max(soil.now$SHARE))[1] # This takes whatever comes first, which is probably a bad idea; this is why the averaging may be the way to go
+  # if(soil.now$ISSOIL[soil.most]==0) next
+  
+  # Take just the dominant soil condition (soil.most) and put the variables we care abotu (vars.hwsd) in the rows for this MU Global soil type
+  spp.sp[rows.mu,vars.hwsd] <- soil.now[soil.most,vars.hwsd]
+}
+summary(spp.sp)
+# You might get warnigns, but it will be okay
+hist(spp.sp$T.SAND)
+
+# Now loop through adding the AWC class value
+for(AWC in unique(spp.sp$AWC.CLASS)){
+  if(is.na(AWC)) next
+  
+  rows.now <- which(spp.sp$AWC.CLASS==AWC)
+  
+  spp.sp[rows.now,"AWC.value"] <- df$D_AWC[df$D_AWC$CODE==AWC,"VALUE"]
+}
+summary(spp.sp)
+spp.sp$SU.SYM74 <- as.factor(spp.sp$SU.SYM74)
+# head(spp.sp[is.na(spp.sp$T.SAND),])
+
+# We might want to do something similar with SU.SYM74 code
+
+
+
+
 
 # We seem to have a lot of texture class 12
 df$D_USDA_TEX_CLASS
