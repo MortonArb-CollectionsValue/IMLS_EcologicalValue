730788a7728153161e6ed555c50efadea64d16bc
Christy Rollinson <crollinson@gmail.com>
2020-12-07 15:33:29 -0600

    BIOME: Added Dataset citation

--- Biome_Extractions.R
+++ Biome_Extractions.R
@@ -4,6 +4,7 @@
 #   https://www.worldwildlife.org/publications/terrestrial-ecoregions-of-the-world
 #   Metadata is in the html file with that download
 #   Actual ecoregion codes are in Table 1 of this publication: https://c402277.ssl.cf1.rackcdn.com/publications/356/files/original/The_Global_200_Priority_Ecoregions_for_Global_Conservation.pdf?1345735162
+# Dataset Citation: Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.
 
 library(ggplot2)
 library(rgdal); library(sp); library(raster)
