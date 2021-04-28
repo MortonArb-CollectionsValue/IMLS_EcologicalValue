8cea84ea609c18ee6d15a3bc73af88fa3421f12d
ShivenS04 <shivenrun@gmail.com>
2021-04-28 16:08:05 -0500

    New analysis script

--- SHIVEN_ANALYSIS.R
+++ SHIVEN_ANALYSIS.R
@@ -9,23 +9,20 @@ Tilia_Climate_Final <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReducti
 Ulmus_Climate_Final <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReductions/Ulmus_Climate_Final.csv")
 
 # PCA 1: kitchen sink approach -- throw it all in
-#Malus PCA
 set.seed(1608)
+#Malus PCA
 malus.pca1 <- prcomp(Malus_Climate_Final[,7:12], center = TRUE,scale. = TRUE)
 save(malus.pca1, )
 
 #Quercus PCA
-set.seed(1608)
 quercus.pca1 <- prcomp(Quercus_Climate_Final[,7:12], center = TRUE,scale. = TRUE)
 save(quercus.pca1, )
 
 #Tilia PCA
-set.seed(1608)
 tilia.pca1 <- prcomp(Tilia_Climate_Final[,7:12], center = TRUE,scale. = TRUE)
 save(tilia.pca1, )
 
 #Ulmus PCA
-set.seed(1608)
 ulmus.pca1 <- prcomp(Ulmus_Climate_Final[,7:12], center = TRUE,scale. = TRUE)
 save(ulmus.pca1, )
 
