8a50958e93c671119c6ff8e8a0c932cab271b6bb
ShivenS04 <shivenrun@gmail.com>
2021-05-24 10:00:59 -0500

    Pre-Meeting Changes

---
+++ 3-1_SoilFinalization.R
@@ -0,0 +1,88 @@
+#Loading in correct packages to extract
+library(devtools)
+# install_github("vqv/ggbiplot") #just used blank line when asked question
+library(ggbiplot)
+library("dplyr"); library("plyr"); library("readr")
+library(ggplot2)
+library(rgdal); library(sp); library(raster)
+library(Hmisc)
+library(data.table)
+
+Genera <- c("Malus", "Quercus", "Tilia", "Ulmus")
+path.dat <- "D:/Data_IMLS_Ecological_Value/PreloadedSoil_Data"
+important.traits <- c("T.GRAVEL", "T.SILT", "T.CLAY", "T.OC", "T.PH.H2O", "T.TEB", "T.ECE", "AWC_VALUE", 
+                      "ROOTS", "T.CEC.CLAY", "T.CEC.SOIL", "T.CACO3", "T.CASO4",	"T.ESP")
+
+#Loading in Data
+Soil_Malus <- read.csv("D:/Data_IMLS_Ecological_Value/PreloadedSoil_Data/Soil_Malus.csv")
+Soil_Quercus <- read.csv("D:/Data_IMLS_Ecological_Value/PreloadedSoil_Data/Soil_Quercus.csv")
+Soil_Tilia <- read.csv("D:/Data_IMLS_Ecological_Value/PreloadedSoil_Data/Soil_Tilia.csv")
+Soil_Ulmus <- read.csv("D:/Data_IMLS_Ecological_Value/PreloadedSoil_Data/Soil_Ulmus.csv")
+
+#Steps in order to put categorical data in dataframe with final reductions
+path.dat <- "D:/Data_IMLS_Ecological_Value/Soil_Extract_Drive/Soil_Extract/"
+important_malus <- data.frame(UID=character(), species_name_acc=character(), decimalLatitude=double(), decimalLongitude=double(), stringsAsFactors=FALSE)
+important_quercus <- data.frame(UID=character(), species_name_acc=character(), decimalLatitude=double(), decimalLongitude=double(), stringsAsFactors=FALSE)
+important_tilia <- data.frame(UID=character(), species_name_acc=character(), decimalLatitude=double(), decimalLongitude=double(), stringsAsFactors=FALSE)
+important_ulmus <- data.frame(UID=character(), species_name_acc=character(), decimalLatitude=double(), decimalLongitude=double(), stringsAsFactors=FALSE)
+
+for (j in 1:length(Genera)) {
+    #MortonArb_Data_path <-  read.csv(file.path(path.dat,"0_MortonArb.csv")) #morton arb values in diff. folders
+    #made each data frame end with original to indicate before NA values were removed
+    #Data Extraction
+    initial_extraction <- list.files(path = path.dat, pattern = Genera[j], full.names=TRUE)
+    extractioncols <- names(read.csv(initial_extraction[1]))
+    coltype <- rep(NA, length(extractioncols))
+    col.char <- which(extractioncols %in% c("nativeDatabaseID", "MU.SOURCE1"))
+    coltype[col.char] <- "character"
+    intermediate_extraction <- lapply(initial_extraction, read.csv, colClasses=coltype) %>% bind_rows()
+    intermediate_extraction <- rbind.fill(intermediate_extraction)
+    intermediate_extraction <- tidyr::separate(intermediate_extraction, col = "species_name_acc", into=c("genus", "species"))
+    final_extraction <- intermediate_extraction[complete.cases(intermediate_extraction[,important.traits]),]
+    final_extraction <- final_extraction[,c(3,4,5,11,12)]
+    if (j==1) {
+      important_malus <- rbind(important_malus, final_extraction)
+      important_malus <- unique(important_malus)
+    } else { if (j==2) {
+      important_quercus <- rbind(important_quercus, final_extraction)
+      important_quercus <- unique(important_quercus)
+    } else { if (j==3) {
+      important_tilia <- rbind(important_tilia, final_extraction)
+      important_tilia <- unique(important_tilia)
+    } else {
+      important_ulmus <- rbind(important_ulmus, final_extraction)
+      important_ulmus <- unique(important_ulmus)
+      }
+    }
+  }
+}
+
+
+#DOES NOT WORK: NOT MATCHING # OF TERMS
+
+#Adding in Categorical Variables & Saving
+Soil_Malus <- Soil_Malus[complete.cases(Soil_Malus), ]
+important_malus <- important_malus[complete.cases(important_malus), ]
+Malus_Soil_Final <- cbind(important_malus, Soil_Malus)
+write.csv(Malus_Soil_Final, "D:/Data_IMLS_Ecological_Value/Total_PostSoilReductions/Malus_Soil_Final.csv", row.names = FALSE)
+
+
+#Adding in Categorical Variables & Saving
+Soil_Quercus <- Soil_Quercus[complete.cases(Soil_Quercus), ]
+important_quercus <- important_quercus[complete.cases(important_quercus), ]
+Quercus_Soil_Final <- cbind(important_quercus, Soil_Quercus)
+write.csv(Quercus_Soil_Final, "D:/Data_IMLS_Ecological_Value/Total_PostSoilReductions/Quercus_Soil_Final.csv", row.names = FALSE)
+
+
+#Adding in Categorical Variables & Saving
+Soil_Tilia <- Soil_Tilia[complete.cases(Soil_Tilia), ]
+important_tilia <- important_tilia[complete.cases(important_tilia), ]
+Tilia_Soil_Final <- cbind(important_tilia, Soil_Tilia)
+write.csv(Tilia_Soil_Final, "D:/Data_IMLS_Ecological_Value/Total_PostSoilReductions/Tilia_Soil_Final.csv", row.names = FALSE)
+
+
+#Adding in Categorical Variables & Saving
+Soil_Ulmus <- Soil_Ulmus[complete.cases(Soil_Ulmus), ]
+important_ulmus <- important_ulmus[complete.cases(important_ulmus), ]
+Ulmus_Soil_Final <- cbind(important_ulmus, Soil_Ulmus)
+write.csv(Ulmus_Soil_Final, "D:/Data_IMLS_Ecological_Value/Total_PostSoilReductions/Ulmus_Soil_Final.csv", row.names = FALSE)
--- SHIVEN_ANALYSIS.R
+++ SHIVEN_ANALYSIS.R
@@ -2,35 +2,52 @@
 library(dplyr); library(ggplot2); library(vegan)
 library(ggbiplot)
 
-#Loading in Data
-Malus_Climate_Final <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReductions/Malus_Climate_Final.csv")
-Quercus_Climate_Final <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReductions/Quercus_Climate_Final.csv")
-Tilia_Climate_Final <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReductions/Tilia_Climate_Final.csv")
-Ulmus_Climate_Final <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReductions/Ulmus_Climate_Final.csv")
+#Loading in Climate Data
+malus.clim <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReductions2/Malus_Climate_Final.csv")
+quercus.clim <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReductions2/Quercus_Climate_Final.csv")
+tilia.clim <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReductions2/Tilia_Climate_Final.csv")
+ulmus.clim <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostReductions2/Ulmus_Climate_Final.csv")
+
+#Loading in Soils Data
+malus.soils <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostSoilReductions/Malus_Soil_Final.csv")
+quercus.soils <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostSoilReductions/Quercus_Soil_Final.csv")
+tilia.soils <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostSoilReductions/Tilia_Soil_Final.csv")
+ulmus.soils <- read.csv("D:/Data_IMLS_Ecological_Value/Total_PostSoilReductions/Ulmus_Soil_Final.csv")
+
+important.traits <- c("T.GRAVEL", "T.SILT", "T.CLAY", "T.OC", "T.PH.H2O", "T.TEB", "T.ECE", "AWC_VALUE", 
+                      "ROOTS", "T.CEC.CLAY", "T.CEC.SOIL", "T.CACO3", "T.CASO4",	"T.ESP")
+
+#Combining Climate & Soil Data
+malus.all <-  merge(malus.clim, malus.soils[,important.traits])
+quercus.all <-  merge(quercus.clim, quercus.soils[,important.traits])
+tilia.all <-  merge(tilia.clim, tilia.soils[,important.traits])
+ulmus.all <-  merge(ulmus.clim, ulmus.soils[,important.traits])
 
 # PCA 1: kitchen sink approach -- throw it all in
 set.seed(1608)
 #Malus PCA
-malus.pca1 <- prcomp(Malus_Climate_Final[,7:12], center = TRUE,scale. = TRUE)
+malus.pca1 <- prcomp(malus.all, center = TRUE,scale. = TRUE)
 summary(malus.pca1)
 save(malus.pca1, file = "D:/Data_IMLS_Ecological_Value/PCAs/maluspca1.RData")
 ggbiplot(malus.pca1) #basic plot
 
 #Quercus PCA
-quercus.pca1 <- prcomp(Quercus_Climate_Final[,7:12], center = TRUE,scale. = TRUE)
+quercus.pca1 <- prcomp(quercus.all, center = TRUE,scale. = TRUE)
 summary(quercus.pca1)
 save(quercus.pca1, file = "D:/Data_IMLS_Ecological_Value/PCAs/quercuspca1.RData")
 ggbiplot(quercus.pca1) #basic plot
 
 #Tilia PCA
-tilia.pca1 <- prcomp(Tilia_Climate_Final[,7:12], center = TRUE,scale. = TRUE)
+tilia.pca1 <- prcomp(tilia.all, center = TRUE,scale. = TRUE)
 summary(tilia.pca1)
 save(tilia.pca1, file = "D:/Data_IMLS_Ecological_Value/PCAs/tiliapca1.RData")
+ggbiplot(tilia.pca1) #basic plot
 
 #Ulmus PCA
-ulmus.pca1 <- prcomp(Ulmus_Climate_Final[,7:12], center = TRUE,scale. = TRUE)
+ulmus.pca1 <- prcomp(ulmus.all, center = TRUE,scale. = TRUE)
 summary(ulmus.pca1)
 save(ulmus.pca1, file = "D:/Data_IMLS_Ecological_Value/PCAs/ulmuspca1.RData")
+ggbiplot(ulmus.pca1) #basic plot
 
 
 # #genus PCA 1
